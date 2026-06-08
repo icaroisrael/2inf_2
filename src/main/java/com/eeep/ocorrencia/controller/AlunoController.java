0af31b38213d4413934814db5931c29100d41786
João <joaopedrobezer12@gmail.com>
2026-06-08 16:28:24 -0300

    controller

--- src/main/java/com/eeep/ocorrencia/controller/AlunoController.java
+++ src/main/java/com/eeep/ocorrencia/controller/AlunoController.java
@@ -1,7 +1,6 @@
 package com.eeep.ocorrencia.controller;
 
 import com.eeep.ocorrencia.model.Aluno;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +11,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/Aluno")
 public class AlunoController {
-    @GetMapping("/aluno")
-    public String abrirFormulario(Model model) {
-        model.addAttribute("aluno", new Aluno());
-        return "cadastro";
 
+    @GetMapping("/listar")
+    public String abrirFormulario(Model model) {
+
+        model.addAttribute("aluno", new Aluno());
+
+        List<Aluno> listarAlunos = new ArrayList<>();
+        model.addAttribute("listarAlunos", listarAlunos);
+
+        return "cadastro";
     }
+
     @PostMapping("/salvar")
-    public String salvarAluno(Aluno aluno){
+    public String salvarAluno(Aluno aluno) {
+
         System.out.println(aluno.getNumero());
         System.out.println(aluno.getNome());
         System.out.println(aluno.getSexo());
         System.out.println(aluno.getTurma());
         System.out.println(aluno.getMatricula());
+
         return "cadastro";
     }
-
 }
