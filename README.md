5b907036998f23001a1f542d444cb4f1d3497cc6
Alana Fernandes <alanalana603@gmail.com>
2026-06-15 11:12:00 -0300

    EquipeTurma1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Equipe5
-## João Pedro, Alicya, Ana kewilly, Sophia
+Equipe 1
+Nome:Vitória Ferreira Cunha de Melo N°42;
+Nome:Alana Fernandes Gabriel N°1;
+Nome:Vitória Gomes da Silva N°43;
+Nome :Isabele de Oliveira Santos N°:16;
+
---
+++ src/main/java/com/eeep/ocorrencia/model/Turma.java
@@ -0,0 +1,33 @@
+package com.eeep.ocorrencia.model;
+
+
+
+    public class Turma {
+
+        private Integer codigo;
+        private String descricao;
+
+        public Turma() {
+        }
+
+        public Turma(Integer codigo, String descricao) {
+            this.codigo = codigo;
+            this.descricao = descricao;
+        }
+
+        public Integer getCodigo() {
+            return codigo;
+        }
+
+        public void setCodigo(Integer codigo) {
+            this.codigo = codigo;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+}
