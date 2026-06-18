f3947f6e9bc206e37a0b337dc1d302800c8c0f75
gabrielasilva13-oss <gabygalaxy969@gmail.com>
2026-06-18 18:15:29 -0300

    Implementa sistema de login

--- src/main/java/com/eeep/ocorrencia/controller/LoginMain.java
+++ src/main/java/com/eeep/ocorrencia/controller/LoginMain.java
@@ -1,4 +1,4 @@
-package com.eeep.ocorrencia;
+package com.eeep.ocorrencia.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
