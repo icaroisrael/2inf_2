53cb966a285c313d952ceefc8deae466574dabe6
João <joaopedrobezer12@gmail.com>
2026-06-18 08:41:30 -0300

    controller

--- src/main/java/com/eeep/ocorrencia/controller/AlunoController.java
+++ src/main/java/com/eeep/ocorrencia/controller/AlunoController.java
@@ -14,25 +14,31 @@ import java.util.List;
 @RequestMapping("/Aluno")
 public class AlunoController {
 
+    private List<Aluno> listarAlunos = new ArrayList<>();
+
     @GetMapping("/listar")
     public String abrirFormulario(Model model) {
 
         model.addAttribute("aluno", new Aluno());
 
-        List<Aluno> listarAlunos = new ArrayList<>();
         model.addAttribute("listarAlunos", listarAlunos);
 
         return "cadastro";
     }
 
     @PostMapping("/salvar")
-    public String salvarAluno(Aluno aluno) {
+    public String salvarAluno(Aluno aluno, Model model) {
 
         System.out.println(aluno.getNumero());
         System.out.println(aluno.getNome());
         System.out.println(aluno.getSexo());
         System.out.println(aluno.getTurma());
         System.out.println(aluno.getMatricula());
+
+        listarAlunos.add(aluno);
+
+        model.addAttribute("aluno", new Aluno());
+        model.addAttribute("listarAlunos", listarAlunos);
 
         return "cadastro";
     }
